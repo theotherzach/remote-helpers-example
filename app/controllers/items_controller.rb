dbc114743d56dea9c47cda02a567ad47a6033e8e
Zach Briggs <briggszj@gmail.com>
2020-05-29 13:00:45 -0500

    Add remote form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,32 @@
 class ItemsController < ApplicationController
-  before_action :set_items, only: :index
+  before_action :set_items, only: [:index, :create]
 
   def index
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to :index, notice: 'Item was successfully created.' }
+        format.js
+        format.json { render json: @item, status: :created, location: @item }
+      else
+        format.html { render :index, status: :unprocessable_entity }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def set_items
-    @items = Item.order(:id).limit(50)
+    @items = Item.all.order(:id).limit(50)
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description)
   end
 end
 
